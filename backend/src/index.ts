1d625228cad58d32d1c39303a58a228c5d4082a5
Mohit <mohithingorani2003@gmail.com>
2025-10-25 04:05:48 +0000

    added better logging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,18 @@
-import { WebSocketServer } from 'ws';
-import { GameManager } from './GameManager';
-import dotenv from "dotenv"
+import { WebSocketServer } from "ws";
+import { GameManager } from "./GameManager";
+import dotenv from "dotenv";
 dotenv.config();
-const wss = new WebSocketServer({ port: Number(process.env.PORT) ||8080 });
+const PORT = Number(process.env.PORT) || 8080;
+const wss = new WebSocketServer({ port: PORT });
 
 const gameManager = new GameManager();
 
+console.log("Websocket server started at port",PORT);
 
-wss.on('connection', function connection(ws) {
-    gameManager.addUser(ws)
+wss.on("connection", function connection(ws) {
+  gameManager.addUser(ws);
 
-    ws.on("disconnect",()=>{
-        gameManager.removeUser(ws);
-    })
+  ws.on("disconnect", () => {
+    gameManager.removeUser(ws);
+  });
 });
